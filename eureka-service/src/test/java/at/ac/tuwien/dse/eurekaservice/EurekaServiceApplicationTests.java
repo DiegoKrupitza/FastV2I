7b81b5574c6da37fdfd6b127868bb0ca4be9cd79
Diego Krupitza <info@diegokrupitza.com>
2022-03-28 19:42:31 +0200

    Eureka server

---
+++ eureka-service/src/test/java/at/ac/tuwien/dse/eurekaservice/EurekaServiceApplicationTests.java
@@ -0,0 +1,11 @@
+package at.ac.tuwien.dse.eurekaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest class EurekaServiceApplicationTests {
+
+	@Test void contextLoads() {
+	}
+
+}
