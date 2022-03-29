c4ee6cf2f329137289404e1c7e54d5d8b79fc22d
Jan Müller <janmueller3698@gmail.com>
2022-03-29 17:12:34 +0200

    feat(tracking-service): configure eslint

--- tracking-service/src/routes.ts
+++ tracking-service/src/routes.ts
@@ -1,8 +1,9 @@
-import { FastifyInstance } from 'fastify'
+import type { FastifyInstance } from 'fastify'
 
 export async function routes(server: FastifyInstance) {
   server.get('/', async (request, reply) => {
     request.log.info('I have been called!')
+    reply.statusCode = 200
     return 'hello world'
   })
 }
