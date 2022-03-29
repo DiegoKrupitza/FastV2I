68775462f35eee318cb02249162854579f785e4b
Diego Krupitza <info@diegokrupitza.com>
2022-03-29 19:20:43 +0200

    changed the user db for services

--- mongodb/mongo-init.js
+++ mongodb/mongo-init.js
@@ -5,7 +5,7 @@ db.createUser(
     {
         user: 'entitySerivce_user',
         pwd: '12345',
-        roles: [{role: 'readWrite', db: 'api_prod_db'}],
+        roles: [{role: 'readWrite', db: 'entitySerivce_db'}],
     },
 );
 
@@ -15,7 +15,7 @@ db.createUser(
     {
         user: 'trackingService_user',
         pwd: '12345',
-        roles: [{role: 'readWrite', db: 'api_prod_db'}],
+        roles: [{role: 'readWrite', db: 'trackingService_db'}],
     },
 );
 
