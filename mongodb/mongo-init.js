4f52c24bde3130ab2b2188a408dd7d086b3a3f54
Diego Krupitza <info@diegokrupitza.com>
2022-03-29 19:29:48 +0200

    changed mongodb script

--- mongodb/mongo-init.js
+++ mongodb/mongo-init.js
@@ -1,22 +1,30 @@
 print('Start #################################################################');
 
-db = db.getSiblingDB('entitySerivce_db');
 db.createUser(
-    {
-        user: 'entitySerivce_user',
-        pwd: '12345',
-        roles: [{role: 'readWrite', db: 'entitySerivce_db'}],
-    },
+        {
+            user: 'entitySerivce_user',
+            pwd: '12345',
+            roles: [
+                {
+                    role: "readWrite",
+                    db: "entitySerivce_db"
+                }
+            ]
+        }
 );
 
 
-db = db.getSiblingDB('trackingService_db');
 db.createUser(
-    {
-        user: 'trackingService_user',
-        pwd: '12345',
-        roles: [{role: 'readWrite', db: 'trackingService_db'}],
-    },
+        {
+            user: 'trackingService_user',
+            pwd: '12345',
+            roles: [
+                {
+                    role: "readWrite",
+                    db: "trackingService_db"
+                }
+            ]
+        }
 );
 
 print('END #################################################################');
