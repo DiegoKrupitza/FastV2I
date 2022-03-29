f8f8c758da1c2b1af0ad0c8fdfea3b4ac3772dc4
Diego Krupitza <info@diegokrupitza.com>
2022-03-29 20:19:02 +0200

    Fixed mongo

---
+++ docker-entrypoint-initdb.d/mongo-init.js
@@ -0,0 +1,23 @@
+print('Start #################################################################');
+
+db = db.getSiblingDB('entitySerivce_db');
+db.createUser(
+  {
+    user: 'entitySerivce_user',
+    pwd: '12345',
+    roles: [{ role: 'readWrite', db: 'entitySerivce_db' }],
+  },
+);
+
+
+db = db.getSiblingDB('trackingService_db');
+db.createUser(
+  {
+    user: 'trackingService_user',
+    pwd: '12345',
+    roles: [{ role: 'readWrite', db: 'trackingService_db' }],
+  },
+);
+
+
+print('END #################################################################');
