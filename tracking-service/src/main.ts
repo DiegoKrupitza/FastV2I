42a50ef3654a7c67d10974ee1684904899bced6b
Jan Müller <janmueller3698@gmail.com>
2022-03-29 18:15:20 +0200

    feat(tracking-service): connect to mongodb

--- tracking-service/src/main.ts
+++ tracking-service/src/main.ts
@@ -1,44 +1,9 @@
-import 'dotenv/config'
-import { Eureka } from 'eureka-js-client'
 import fastify from 'fastify'
 
+import { connectToDatabase } from './database'
+import { env } from './env'
+// import { eureka } from './eureka'
 import { routes } from './routes'
-
-const port = parseInt(process.env.port ?? '8888', 10)
-const host = '0.0.0.0'
-
-const eureka = new Eureka({
-  instance: {
-    app: 'tracking-service',
-    hostName: process.env.HOSTNAME ?? 'localhost',
-    ipAddr: host,
-    port: {
-      $: port,
-      '@enabled': true,
-    },
-    vipAddress: 'tracking-service',
-    dataCenterInfo: {
-      name: 'MyOwn',
-      '@class': 'com.netflix.appinfo.InstanceInfo$DefaultDataCenterInfo',
-    },
-  },
-  eureka: {
-    host: process.env.EUREKA_SERVER_ADDRESS,
-    port: 8761,
-    servicePath: '/eureka/apps',
-  },
-})
-
-function startEureka() {
-  eureka.start((err) => {
-    if (err) {
-      console.warn('Could not start Eureka client. Retrying in 5 seconds.')
-      setTimeout(() => startEureka(), 5000)
-    }
-  })
-}
-
-startEureka()
 
 const server = fastify({
   logger: {
@@ -51,11 +16,13 @@ const server = fastify({
 })
 
 server.register(routes)
+// server.register(eureka)
 
-server.listen({ port, host }, (err) => {
+connectToDatabase(server)
+
+server.listen({ port: env.port, host: env.host }, (err) => {
   if (err) {
     console.error(err)
-    eureka.stop()
     process.exit(1)
   }
 })
