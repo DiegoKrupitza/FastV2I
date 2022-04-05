9f6f4add9d0c10f44c952c1c3f8a9476dca4a6f9
Jan Müller <janmueller3698@gmail.com>
2022-04-05 20:13:53 +0200

    feat(entity-service): persist data of incoming entity messages

--- entity-service/src/amqp.ts
+++ entity-service/src/amqp.ts
@@ -1,18 +1,98 @@
-import type { Channel } from 'amqplib'
+import type { Channel, ConsumeMessage } from 'amqplib'
 import { connect } from 'amqplib'
 import type { FastifyInstance } from 'fastify'
 
+import { collections } from './database'
+import type { CarDto } from './model/car'
+import { Mappers } from './model/mappers'
+import type { TrafficLightDto } from './model/traffic-light'
+
+const CAR = 'car'
+const TRAFFIC_LIGHT = 'traffic-light'
+
 export const amqp: { channel?: Channel } = {}
 
-export async function connectToAmqp(server: FastifyInstance) {
+export async function connectToAmqp(
+  server: FastifyInstance,
+  retryDelay = 1000
+) {
   try {
-    const url = `amqp://${process.env.RABBIT_MQ_HOST}`
-    const connection = await connect(url)
+    const hostname = process.env.RABBIT_MQ_HOST
+    const connection = await connect({ hostname })
     amqp.channel = await connection.createChannel()
-    server.log.info(`Connected to amqp at ${url}`)
-    await amqp.channel.assertQueue('cars')
-    amqp.channel.sendToQueue('cars', Buffer.from('hello from entity-service'))
+    server.log.info(`[amqp] Connected to ${hostname}.`)
+    await amqp.channel.assertQueue(CAR)
+    await amqp.channel.assertQueue(TRAFFIC_LIGHT)
+    amqp.channel.consume(CAR, (msg) => onCarMessage(msg, server))
+    amqp.channel.consume(TRAFFIC_LIGHT, (msg) =>
+      onTrafficLightMessage(msg, server)
+    )
   } catch (err) {
-    server.log.error(err)
+    server.log.error(
+      `[amqp] Connection failed. Retrying in ${
+        retryDelay / 1000
+      } second(s). ${err}`
+    )
+    setTimeout(() => connectToAmqp(server, retryDelay + 1000), retryDelay)
   }
+}
+
+async function onCarMessage(
+  msg: ConsumeMessage | null,
+  server: FastifyInstance
+): Promise<void> {
+  if (!msg) {
+    return
+  }
+  server.log.info(`[amqp] Received message "${msg.content.toString()}"`)
+  const cars = collections?.cars
+  if (!cars) {
+    return
+  }
+  try {
+    const car = JSON.parse(msg.content.toString())
+    if (!isValidCar(car)) {
+      return
+    }
+    cars.insertOne(Mappers.carDtoToCar(car))
+  } catch (err) {
+    server.log.error(
+      `[amqp] Could not persist car state "${msg.content.toString()}"`
+    )
+  }
+}
+
+function isValidCar(object: any): object is CarDto {
+  // TODO
+  return true
+}
+
+async function onTrafficLightMessage(
+  msg: ConsumeMessage | null,
+  server: FastifyInstance
+): Promise<void> {
+  if (!msg) {
+    return
+  }
+  server.log.info(`[amqp] Received message "${msg.content.toString()}"`)
+  const trafficLights = collections?.trafficLights
+  if (!trafficLights) {
+    return
+  }
+  try {
+    const trafficLight = JSON.parse(msg.content.toString())
+    if (!isValidTrafficLight(trafficLight)) {
+      return
+    }
+    trafficLights.insertOne(Mappers.trafficLightDtoToTrafficLight(trafficLight))
+  } catch (err) {
+    server.log.error(
+      `[amqp] Could not persist traffic light state "${msg.content.toString()}"`
+    )
+  }
+}
+
+function isValidTrafficLight(object: any): object is TrafficLightDto {
+  // TODO
+  return true
 }
--- entity-service/test/test-utils.ts
+++ entity-service/test/test-utils.ts
@@ -1,14 +1,48 @@
 import type { FastifyInstance } from 'fastify'
 import { MongoMemoryServer } from 'mongodb-memory-server'
 
+import { collections } from '../src/database'
+import { Mappers } from '../src/model/mappers'
 import { createServer } from '../src/server'
 
 const createTestServer = async (): Promise<FastifyInstance> => {
   const mongod = await MongoMemoryServer.create()
-  return createServer({
+  const server = await createServer({
     isTest: true,
     mongoDbUrl: mongod.getUri(),
   })
+  await insertTestData()
+  return server
+}
+
+async function insertTestData() {
+  await collections.cars?.insertMany(
+    [
+      {
+        vin: 'vw-passat-1',
+        oem: 'vw',
+        model: 'passat',
+      },
+      {
+        vin: 'vw-polo-1',
+        oem: 'vw',
+        model: 'polo',
+      },
+    ].map(Mappers.carDtoToCar)
+  )
+
+  await collections.trafficLights?.insertMany(
+    [
+      {
+        id: 'tl-1',
+        location: [1, 1],
+      },
+      {
+        id: 'tl-2',
+        location: [42, 7],
+      },
+    ].map(Mappers.trafficLightDtoToTrafficLight)
+  )
 }
 
 export const TestUtils = {
