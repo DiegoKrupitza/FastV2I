04682fee05980262631a63a09a8036a018316d8d
Jan Müller <janmueller3698@gmail.com>
2022-04-21 20:39:04 +0200

    feat(frontend): improve state polling

--- cockpit/src/composables/simulation-state.ts
+++ cockpit/src/composables/simulation-state.ts
@@ -1,67 +1,71 @@
+import type { Ref } from 'vue'
+
 import { useBackend } from '~/composables'
+import type {
+  Car,
+  CarEntity,
+  CarState,
+  TrafficLight,
+  TrafficLightEntity,
+  TrafficLightState,
+} from '~/types'
 
-export interface CarEntity {
-  vin: string
-  oem: string
-  model: string
+const cars = ref<Car[]>([])
+const trafficLights = ref<TrafficLight[]>([])
+
+export interface UseSimulationState {
+  cars: Ref<Car[]>
+  trafficLights: Ref<TrafficLight[]>
 }
 
-export interface CarState {
-  vin: string
-  timestamp: string
-}
-
-export interface TrafficLightEntity {
-  id: string
-  location: [number, number]
-}
-
-export interface TrafficLightState {
-  id: string
-  color: 'green' | 'red'
-  timestamp: string
-}
-
-const carEntities = ref<CarEntity[]>([])
-const carStates = ref<{ car: CarEntity; state: CarState }[]>([])
-const trafficLightEntities = ref<TrafficLightEntity[]>()
-const trafficLightStates =
-  ref<{ trafficLight: TrafficLightEntity; state: TrafficLightState }[]>()
-
-export function useSimulationState() {
+async function fetchCars() {
   const { get } = useBackend()
+  const carEntities = (await get<CarEntity[]>('/entities/cars')).data
+  cars.value = await Promise.all(
+    carEntities.map(async (car) => {
+      const latest = await get<CarState>(`/tracking/cars/${car.vin}/latest`)
+      return { ...car, ...latest.data }
+    })
+  )
+}
+
+async function fetchTrafficLights() {
+  const { get } = useBackend()
+  const trafficLightEntities = (
+    await get<TrafficLightEntity[]>('/entities/traffic-lights')
+  ).data
+  trafficLights.value = await Promise.all(
+    trafficLightEntities.map(async (trafficLight) => {
+      const latest = await get<TrafficLightState>(
+        `/tracking/traffic-lights/${trafficLight.id}/latest`
+      )
+      return { ...trafficLight, ...latest.data }
+    })
+  )
+}
+
+export function useSimulationState(pollingRate = 5000): UseSimulationState {
+  let requestInProgess = false
 
   useIntervalFn(
     async () => {
-      const cars = (await get<CarEntity[]>('/entities/cars')).data
-      carEntities.value = cars
-      carStates.value = await Promise.all(
-        cars.map(async (car) => {
-          const latest = await get<CarState>(`/tracking/cars/${car.vin}/latest`)
-          return { car, state: latest.data }
-        })
-      )
-      const trafficLights = (
-        await get<TrafficLightEntity[]>('/entities/traffic-lights')
-      ).data
-      trafficLightEntities.value = trafficLights
-      trafficLightStates.value = await Promise.all(
-        trafficLights.map(async (trafficLight) => {
-          const latest = await get<TrafficLightState>(
-            `/tracking/traffic-lights/${trafficLight.id}/latest`
-          )
-          return { trafficLight, state: latest.data }
-        })
-      )
+      if (requestInProgess) {
+        return
+      }
+      requestInProgess = true
+      try {
+        await Promise.all([fetchCars(), fetchTrafficLights()])
+      } catch (e) {
+        console.error(e)
+      }
+      requestInProgess = false
     },
-    1000,
+    pollingRate,
     { immediateCallback: true }
   )
 
   return {
-    carEntities,
-    carStates,
-    trafficLightEntities,
-    trafficLightStates,
+    cars,
+    trafficLights,
   }
 }
--- cockpit/src/pages/index.vue
+++ cockpit/src/pages/index.vue
@@ -1,52 +1,35 @@
 <script setup lang="ts">
 import { useSimulationState } from '~/composables'
 
-const { carEntities, carStates, trafficLightEntities, trafficLightStates } =
-  useSimulationState()
+const { cars, trafficLights } = useSimulationState()
 </script>
 
 <template>
   <h1>State</h1>
   <div class="pa-4 gap-4">
     <section>
-      <h2>Entities</h2>
-      <h3>Cars</h3>
-      <ul>
-        <li v-for="car of carEntities" :key="car.vin">{{ car }}</li>
-      </ul>
-      <h3>Traffic Lights</h3>
-      <ul>
-        <li v-for="trafficLight of trafficLightEntities" :key="trafficLight.id">
-          {{ trafficLight }}
-        </li>
-      </ul>
-    </section>
-    <section>
       <h2>Tracking</h2>
       <h3>Cars</h3>
-      <div v-for="{ car, state } of carStates" :key="car.vin">
+      <div v-for="car of cars" :key="car.vin">
         <h4>{{ car.vin }}</h4>
         <ul>
           <li>
-            {{ state }}
+            {{ car }}
           </li>
         </ul>
       </div>
       <h3>Traffic Lights</h3>
-      <div
-        v-for="{ trafficLight, state } of trafficLightStates"
-        :key="trafficLight.id"
-      >
+      <div v-for="trafficLight of trafficLights" :key="trafficLight.id">
         <h4>{{ trafficLight.id }}</h4>
         <ul>
           <li>
-            {{ state }}
             <carbon-circle-solid
               :class="{
-                'color-green400': state.color === 'green',
-                'color-red': state.color === 'red',
+                'color-green400': trafficLight.color === 'green',
+                'color-red': trafficLight.color === 'red',
               }"
             />
+            {{ trafficLight }}
           </li>
         </ul>
       </div>
