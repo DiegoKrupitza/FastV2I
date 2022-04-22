05e05abec89a591e24d2500ed74c5f012487ee71
Diego Krupitza <info@diegokrupitza.com>
2022-04-22 21:41:14 +0200

    feat: started with the flow control logic

--- flowcontrol-service/src/main/java/at/ac/tuwien/dse/flowcontrolservice/service/EntityServiceFeign.java
+++ flowcontrol-service/src/main/java/at/ac/tuwien/dse/flowcontrolservice/service/EntityServiceFeign.java
@@ -1,11 +1,20 @@
 package at.ac.tuwien.dse.flowcontrolservice.service;
 
+import at.ac.tuwien.dse.flowcontrolservice.dto.NearestTrafficLightDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.Optional;
 
 @FeignClient(name = "entity-service")
 public interface EntityServiceFeign {
 
   @DeleteMapping("/entities/all")
   void resetAll();
+
+  @GetMapping("/entities/traffic-lights/near/{location}/{direction}")
+  Optional<NearestTrafficLightDto> getNearestTrafficLight(
+      @PathVariable("location") Long location, @PathVariable("direction") String direction);
 }
--- flowcontrol-service/src/main/java/at/ac/tuwien/dse/flowcontrolservice/service/FlowControlService.java
+++ flowcontrol-service/src/main/java/at/ac/tuwien/dse/flowcontrolservice/service/FlowControlService.java
@@ -2,17 +2,35 @@ package at.ac.tuwien.dse.flowcontrolservice.service;
 
 import at.ac.tuwien.dse.flowcontrolservice.config.FlowControlProperties;
 import at.ac.tuwien.dse.flowcontrolservice.dto.CarStateDto;
+import at.ac.tuwien.dse.flowcontrolservice.dto.NearestTrafficLightDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class FlowControlService {
 
   private final FlowControlProperties flowControlProperties;
+  private final EntityServiceFeign entityServiceFeign;
 
+  /**
+   * Calculates the perfect speed for the car to drive
+   *
+   * @param car the car
+   * @return the perfect speed
+   */
   public Long getAdvisedSpeed(CarStateDto car) {
-    // TODO: logic
+    Optional<NearestTrafficLightDto> nearestTrafficLight =
+        entityServiceFeign.getNearestTrafficLight(car.location()[0], car.directionCode());
+
+    if (nearestTrafficLight.isEmpty()) {
+      return car.speed();
+    }
+
+    // TODO: logic für fancy stuff
+
     return car.speed();
   }
 }
