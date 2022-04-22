c4b42b7b22020d5c8dd949b743518ffc56a6834c
Diego Krupitza <info@diegokrupitza.com>
2022-04-22 12:29:12 +0200

    feat(simulator): added direction to car-state messages

--- simulator-service/src/main/java/at/ac/dse/simulatorservice/simulator/mapper/CarMapper.java
+++ simulator-service/src/main/java/at/ac/dse/simulatorservice/simulator/mapper/CarMapper.java
@@ -2,16 +2,18 @@ package at.ac.dse.simulatorservice.simulator.mapper;
 
 import at.ac.dse.simulatorservice.domain.Car;
 import at.ac.dse.simulatorservice.simulator.domain.CarStateMom;
+import at.ac.dse.simulatorservice.simulator.domain.Direction;
 
 import java.time.LocalDateTime;
 
 public abstract class CarMapper {
 
-  public static CarStateMom toCarStateMom(Car car) {
+  public static CarStateMom toCarStateMom(Car car, Direction direction) {
     return new CarStateMom(
         car.getVin(),
         new Long[] {car.getLocation(), 0L},
         car.getSpeed(),
+        direction.getName(),
         LocalDateTime.now());
   }
 }
