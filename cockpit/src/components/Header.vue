51ccb912c7face550288fd38cfb29cf6b624030c
Jan Müller <janmueller3698@gmail.com>
2022-04-23 15:56:22 +0200

    feat(frontend): make visualization fullscreen

--- cockpit/src/components/Header.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <header
-    class="flex-center flex-col position-sticky top-0 left-0 bg-gray200 dark:bg-black shadow-md z-42"
-  >
-    <Navigation />
-  </header>
-</template>
--- cockpit/src/layouts/default.vue
+++ cockpit/src/layouts/default.vue
@@ -1,5 +1,4 @@
 <template>
-  <Header />
   <Main>
     <router-view />
   </Main>
