78c3964e9bab28cb6821a592ac1def8d1a5bb132
Jan Müller <janmueller3698@gmail.com>
2022-04-23 14:42:12 +0200

    feat(frontend): improve scaling

--- cockpit/src/components/simulation/Car.vue
+++ cockpit/src/components/simulation/Car.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { useSimulationVisualization } from '~/composables'
 import type { Car, Simulation } from '~/types'
 
 const props = defineProps<{ car: Car; simulation: Simulation }>()
@@ -7,15 +8,18 @@ const { car } = toRefs(props)
 const emit = defineEmits<{
   (eventName: 'select', car: Car): void
 }>()
+
+const { actorSize, height } = useSimulationVisualization()
 </script>
 
 <template>
   <g>
     <circle
       :cx="car.location"
-      :cy="50"
-      :r="100"
-      class="fill-blue stroke-16 stroke-black"
+      :cy="0.5 * height"
+      :r="actorSize"
+      class="fill-blue stroke-8 stroke-black"
+      :style="{ 'stroke-width': actorSize / 5 }"
       @click="emit('select', car)"
     />
   </g>
