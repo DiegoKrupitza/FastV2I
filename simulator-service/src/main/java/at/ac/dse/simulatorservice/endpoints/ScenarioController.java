df3afd3252b683082a7db048eae94cf979be906d
Diego Krupitza <info@diegokrupitza.com>
2022-04-24 17:23:41 +0200

    feat(simulator): tests for simulator

--- simulator-service/src/main/java/at/ac/dse/simulatorservice/endpoints/ScenarioController.java
+++ simulator-service/src/main/java/at/ac/dse/simulatorservice/endpoints/ScenarioController.java
@@ -4,12 +4,14 @@ import at.ac.dse.simulatorservice.dtos.ScenarioDto;
 import at.ac.dse.simulatorservice.services.SimulatorService;
 import at.ac.dse.simulatorservice.services.ValidatorService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/simulator")
 @RequiredArgsConstructor
+@Slf4j
 public class ScenarioController {
 
   private final SimulatorService simulatorService;
@@ -29,6 +31,7 @@ public class ScenarioController {
 
   @DeleteMapping
   public void reset() {
+    log.info("Reset the simulator requested");
     this.simulatorService.resetSimulation();
   }
 }
