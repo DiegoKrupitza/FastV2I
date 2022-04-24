8b6247b02ad13b015bae546cabdfccc58089b1c4
Diego Krupitza <info@diegokrupitza.com>
2022-04-24 11:11:48 +0200

    feat(simulator): simulation is only allowed to start if there is no sim running

---
+++ simulator-service/src/main/java/at/ac/dse/simulatorservice/excetion/ApiError.java
@@ -0,0 +1,31 @@
+package at.ac.dse.simulatorservice.excetion;
+
+import org.springframework.http.HttpStatus;
+
+import java.time.LocalDateTime;
+
+public record ApiError(Integer status, String description, String time) {
+
+    /**
+     * Generates a ApiError object from the given exception and the response status
+     *
+     * @param e the exception to wrap
+     * @param responseStatus the status
+     * @return the newly created ApiError object
+     */
+    public static ApiError from(Exception e, HttpStatus responseStatus) {
+        return ApiError.from(e.getMessage(), responseStatus);
+    }
+
+    /**
+     * Generates a ApiError object from the given message and the response status
+     *
+     * @param msg the message to wrap
+     * @param responseStatus the status
+     * @return the newly created ApiError object
+     */
+    public static ApiError from(String msg, HttpStatus responseStatus) {
+        return new ApiError(responseStatus.value(), msg, LocalDateTime.now().toString());
+    }
+}
+
