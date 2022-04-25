c51c01db83aa9aacbcc4d898b0f4505f5e242fce
Kian <icdef007@gmail.com>
2022-04-25 23:28:31 +0200

    can try out swagger requests in ui also for simulator

--- simulator-service/src/main/java/at/ac/dse/simulatorservice/SimulatorServiceApplication.java
+++ simulator-service/src/main/java/at/ac/dse/simulatorservice/SimulatorServiceApplication.java
@@ -2,15 +2,13 @@ package at.ac.dse.simulatorservice;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
-@OpenAPIDefinition(servers = @Server(),
-        info =
+@OpenAPIDefinition(info =
 @Info(title = "Simulator API", version = "1.0", description = "Simulator Employee API v1.0")
 )
 public class SimulatorServiceApplication {
