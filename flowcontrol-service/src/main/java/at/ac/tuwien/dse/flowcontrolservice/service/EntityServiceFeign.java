4a9e166d50a611af981b195f0eb276a72177baf3
Diego Krupitza <info@diegokrupitza.com>
2022-04-25 14:38:58 +0200

    chore(flow-control): javadoc for the flow control service

--- flowcontrol-service/src/main/java/at/ac/tuwien/dse/flowcontrolservice/service/EntityServiceFeign.java
+++ flowcontrol-service/src/main/java/at/ac/tuwien/dse/flowcontrolservice/service/EntityServiceFeign.java
@@ -8,12 +8,22 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.Optional;
 
+/** Feign client to interact with the entity-service. */
 @FeignClient(name = "entity-service")
 public interface EntityServiceFeign {
 
+  /** Resets the entity-serice to a state where no data is registered. */
   @DeleteMapping("/entities/all")
   void resetAll();
 
+  /**
+   * Gets the nearest traffic light from a given position considering the direction we are looking.
+   *
+   * @param location the location from where the nearest location should be calculated.
+   * @param direction the direction we are looking
+   * @return the nearest traffic light. If there is no nearest traffic light the return value is
+   *     {@code Optional.EMPTY}
+   */
   @GetMapping("/entities/traffic-lights/near/{location}/{direction}")
   Optional<NearestTrafficLightDto> getNearestTrafficLight(
       @PathVariable("location") Long location, @PathVariable("direction") String direction);
