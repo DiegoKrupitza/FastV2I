d50d327a45c3cafce175bb66d003599d32ab6770
Diego Krupitza <info@diegokrupitza.com>
2022-04-27 12:37:02 +0200

    Fixed simulator registry in docker and added schema definition for traffic light and car

--- simulator-service/src/main/java/at/ac/dse/simulatorservice/domain/Car.java
+++ simulator-service/src/main/java/at/ac/dse/simulatorservice/domain/Car.java
@@ -1,5 +1,6 @@
 package at.ac.dse.simulatorservice.domain;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,23 +15,33 @@ import javax.validation.constraints.Min;
 public class Car {
 
   @Length(min = 1)
+  @Schema(description = "The vin of the car", nullable = false, example = "V1")
   private String vin;
 
   @Length(min = 1)
+  @Schema(description = "The oem of the car", nullable = false, example = "Tesla")
   private String oem;
 
   @Length(min = 1)
+  @Schema(description = "The model of the car", nullable = false, example = "Model 3")
   private String model;
 
   @Min(0)
+  @Schema(
+      description = "After how many ms the car should enter the scene",
+      nullable = false,
+      example = "1000")
   private Long entryTime;
 
   @Min(0)
+  @Schema(description = "The speed of the car", nullable = false)
   private Long speed;
 
   @Min(0)
+  @Schema(description = "The start location of the car", nullable = false, example = "0")
   private Long location;
 
   @Min(0)
+  @Schema(description = "The desired destination of the car", nullable = false, example = "1800")
   private Long destination;
 }
