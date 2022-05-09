03d4216bd0d8c0379df4053e768c2f8c6e30333f
Diego Krupitza <info@diegokrupitza.com>
2022-05-09 20:59:05 +0200

    Transformed from eureka to kubernetes

--- gateway/src/main/java/at/ac/tuwien/dse/gateway/ServiceAvailableController.java
+++ gateway/src/main/java/at/ac/tuwien/dse/gateway/ServiceAvailableController.java
@@ -1,5 +1,7 @@
 package at.ac.tuwien.dse.gateway;
 
+import at.ac.tuwien.dse.gateway.service.HealthService;
+import at.ac.tuwien.dse.gateway.service.ServiceDto;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -8,65 +10,55 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-import java.util.function.Function;
-import java.util.stream.Collectors;
+import java.util.Map;
 import java.util.stream.Stream;
 
-/**
- * Controller for the service resource
- */
+/** Controller for the service resource */
 @RequestMapping("services")
 @RequiredArgsConstructor
 @Slf4j
 @RestController
 @CrossOrigin
-@Tag(name = "ServiceAvailableController", description = "Endpoint for getting status information about services")
+@Tag(
+    name = "ServiceAvailableController",
+    description = "Endpoint for getting status information about services")
 public class ServiceAvailableController {
 
-    private final DiscoveryClient discoveryClient;
+  private final HealthService healthService;
 
-    private final List<String> serviceNames = List.of("ENTITY-SERVICE", "TRACKING-SERVICE", "SIMULATOR-SERVICE", "FLOWCONTROL-SERVICE");
+  private static final Map<String, String> SERVICE_LIST =
+      Map.of(
+          "entity-service", "http://entity-service:8889/entities/health/", //
+          "tracking-service", "http://tracking-service:8888/tracking/health", //
+          "simulator-service", "http://simulator-service:8081/simulator/health", //
+          "flowcontrol-service", "http://flowcontrol-service:8087/flowcontrol/health" //
+          );
 
-    /**
-     * Lists the health status of the services our gateway is interacting with.
-     *
-     * @return the status of our services.
-     */
-    @GetMapping
-    @Operation(summary = "Get status of all services",
-            description = "Returns the status of all services",
-            responses = @ApiResponse(responseCode = "200", description = "Successful response",
-                    content = @Content(mediaType = "application/json",
-                            array = @ArraySchema(schema = @Schema(implementation = ServiceDto.class)))))
-    public Stream<ServiceDto> getAllHealthyServices() {
+  /**
+   * Lists the health status of the services our gateway is interacting with.
+   *
+   * @return the status of our services.
+   */
+  @GetMapping
+  @Operation(
+      summary = "Get status of all services",
+      description = "Returns the status of all services",
+      responses =
+          @ApiResponse(
+              responseCode = "200",
+              description = "Successful response",
+              content =
+                  @Content(
+                      mediaType = "application/json",
+                      array = @ArraySchema(schema = @Schema(implementation = ServiceDto.class)))))
+  public Stream<ServiceDto> getAllHealthyServices() {
 
-        return serviceNames
-                .stream() //
-                .collect( //
-                        Collectors.toMap(Function.identity(), discoveryClient::getInstances) //
-                ) //
-                .entrySet() //
-                .stream() //
-                .map(item ->  //
-                        new ServiceDto(item.getKey(), !CollectionUtils.isEmpty(item.getValue())) //
-                );
-    }
-
-}
-
-/**
- * Descirbes the state of a service.
- *
- * @param name the name of the service.
- * @param up   <code>true</code> if the service is alive otherwise <code>false</code>.
- */
-record ServiceDto(@Schema(description = "Name of service") String name,@Schema(description = "True if service is alive, otherwise false") boolean up) {
+    return SERVICE_LIST.entrySet().stream()
+        .map(item -> this.healthService.getHealthStatus(item.getKey(), item.getValue()));
+  }
 }
